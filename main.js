c5accc13ad554ca363d8b11c7432879a082a363d
ArenVosk <84827064+ArenVosk@users.noreply.github.com>
2022-03-08 17:37:16 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function bedroom(){
+    window.location = "index_bedroom.html";
+}
+
+function bottles(){
+    window.location = "index_bottles.html";
+}
+
+function desk(){
+    window.location = "index_desk.html";
+}
+
+function fruit_basket(){
+    window.location = "index_fruit_basket.html";
+}
+
+function tv_ac(){
+    window.location = "index_tv_ac.html";
+}
